59026f17ed205cff25bc3669e3b90946ea6b59f8
ChrstphGrnr <cgruener@quantummetric.com>
2020-04-03 09:15:42 -0600

    add more standard test functions

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -46,4 +46,24 @@ test.only('My First TestCafe Test', async t => {
 	await t.navigateTo('url')
 	await t.takeScreenshot()
 	await t.resizeWindow(800, 600) // first argument is width, second is height
+
+	// Deep Equal
+	await t.expect('foo').eql('foo', 'message', options) // error message//
+	// Not Deep Equal
+	await t.expect('foo').notEql('Foo')
+	// OK
+	await t.expect('foo').ok() // works like a conditional, 'if "foo" is truthy, it will return true
+	// Not OK
+	await t.expect('foo').notOk()
+	// contains
+	await t.expect('foo').contains('o')
+	// Not contains
+	await t.expect('foo').notContains('F')
+	// Greater or Less than
+	await t.expect(10).gt(4)
+	await t.expect(10).gte(10)
+	await t.expect(10).lt(12)
+	await t.expect(10).lte(10)
+	// Within
+	await t.expect(10).within(1, 100)
 })
