8ac8975411968d3db22b98c970177a7ee7ce6859
ChrstphGrnr <cgruener@quantummetric.com>
2020-04-06 13:47:43 -0600

    add another test for emailed password recovery

---
+++ tests/forgotten-password.test.js
@@ -0,0 +1,23 @@
+import { Selector } from 'testcafe'
+
+//prettier-ignore
+fixture`Send forgotten password test`
+    .page`http://zero.webappsecurity.com/index.html`
+
+test('User can request new password to be sent through his email', async (t) => {
+	// Selectors
+	const signInButton = Selector('#signin_button')
+	const linkToForgottenPassword = Selector('a').withText('Forgot your password')
+	const emailInput = Selector('#user_email')
+	const pageMessage = Selector('div').innerText
+
+	//Actions
+	await t.click(signInButton)
+	await t.click(linkToForgottenPassword)
+	await t.typeText(emailInput, 'email@email.com', { paste: true })
+	await t.pressKey('enter')
+
+	//Assertions
+	await t.expect(pageMessage).contains('email@email.com')
+	await t.expect(emailInput.exist).notOk()
+})
