721ec919054360dee5f84cbde207cd1f69b37b1d
ChrstphGrnr <cgruener@quantummetric.com>
2020-04-06 13:29:14 -0600

    change node version

--- tests/login.test.js
+++ tests/login.test.js
@@ -4,7 +4,7 @@ import { Selector } from 'testcafe'
 fixture`Login Test`
     .page`http://zero.webappsecurity.com/`
 
-test('User cannot login with invalid credentials', async t => {
+test('User cannot login with invalid credentials', async (t) => {
 	const signInButton = Selector('#signin_button')
 	await t.click(signInButton)
 
@@ -23,23 +23,25 @@ test('User cannot login with invalid credentials', async t => {
 	await t.expect(loginError.exists).ok()
 })
 
-// test('User CAN log in with valid credentials', async t => {
-// 	const signInButton = Selector('#signin_button')
-// 	await t.click(signInButton)
+test('User cannot login with invalid credentials', async (t) => {
+	const signInButton = Selector('#signin_button')
+	await t.click(signInButton)
 
-// 	const loginForm = Selector('#login_form')
-// 	await t.expect(loginForm.exists).ok()
+	const loginForm = Selector('#login_form')
+	await t.expect(loginForm.exists).ok()
 
-// 	const usernameInput = Selector('#user_login')
-// 	const userPassword = Selector('#user_password')
-// 	const submitButton = Selector('.btn-primary')
-// 	await t.typeText(usernameInput, 'username', { paste: true })
-// 	await t.typeText(userPassword, 'password', { paste: true })
+	const usernameInput = Selector('#user_login')
+	const userPassword = Selector('#user_password')
 
-// 	await t.debug()
+	await t.typeText(usernameInput, 'username')
+	await t.typeText(userPassword, 'password')
 
-// 	await t.click(submitButton)
+	await t.debug()
 
-// 	const loginError = Selector('#login_form > div.alert.alert-error')
-// 	await t.expect(loginError.exists).ok()
-// })
+	const submitButton = Selector('#login_form > div.form-actions > input')
+	await t.click(submitButton)
+
+	const accountSummaryTab = Selector('#account_summary_tab')
+	await t.click(accountSummaryTab)
+	await t.expect(loginForm.exists).notOk()
+})
